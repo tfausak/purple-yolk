4531be15185c7f8bc8820ebc7dc2225021c6c2a7
Taylor Fausak <taylor@fausak.me>
2019-11-30 12:22:13 -0500

    Initialize connection and listen for file save events

--- server/Main.js
+++ server/Main.js
@@ -5,7 +5,14 @@ const vscode = require('vscode-languageserver');
 
 exports.connection_create = () => vscode.createConnection();
 
-exports.connection_listen = (x) => () => x.listen();
+exports.connection_listen = (connection) => () =>
+  connection.listen();
+
+exports.connection_onDidSaveTextDocument = (connection) => (f) => () =>
+  connection.onDidSaveTextDocument((event) => f(event)());
+
+exports.connection_onInitialize = (connection) => (f) => () =>
+  connection.onInitialize(f);
 
 exports.io_bind = (x) => (f) => () => f(x())();
 
--- server/Main.purs
+++ server/Main.purs
@@ -1,4 +1,19 @@
-module Main ( IO, main ) where
+module Main ( IO, Unit, main ) where
+
+applyBackward :: forall a b . (a -> b) -> a -> b
+applyBackward f x = f x
+
+infixr 1 applyBackward as <|
+
+newtype Unit = Unit {}
+
+unit :: Unit
+unit = Unit {}
+
+newtype Uri = Uri String
+
+uriToString :: Uri -> String
+uriToString (Uri string) = string
 
 foreign import data IO :: Type -> Type
 
@@ -12,9 +27,19 @@ foreign import data Connection :: Type
 
 foreign import connection_create :: IO Connection
 
-foreign import connection_listen :: Connection -> IO {}
+foreign import connection_listen :: Connection -> IO Unit
 
-foreign import log :: String -> IO {}
+foreign import connection_onDidSaveTextDocument
+  :: Connection
+  -> ({ textDocument :: { uri :: Uri, version :: Int } } -> IO Unit)
+  -> IO Unit
+
+foreign import connection_onInitialize
+  :: Connection
+  -> IO { capabilities :: { textDocumentSync :: { save :: Boolean } } }
+  -> IO Unit
+
+foreign import log :: String -> IO Unit
 
 class HasMap t where
   map :: forall a b . (a -> b) -> t a -> t b
@@ -34,11 +59,18 @@ class HasBind t where
 instance ioHasBind :: HasBind IO where
   bind = io_bind
 
-discard :: forall t b . HasBind t => t {} -> ({} -> t b) -> t b
+discard :: forall t b . HasBind t => t Unit -> (Unit -> t b) -> t b
 discard = bind
 
-main :: IO {}
+main :: IO Unit
 main = do
   connection <- connection_create
+
+  connection_onDidSaveTextDocument connection \ event ->
+    log (uriToString event.textDocument.uri)
+
+  connection_onInitialize connection <| pure
+    { capabilities: { textDocumentSync: { save: true } } }
+
   connection_listen connection
-  log "Hello from PureScript!"
+  log "Purple Yolk is up and running!"
