f740a114b48e56dee0f582bfb0ef38170b9b6930
Taylor Fausak <taylor@fausak.me>
2019-12-01 08:28:29 -0500

    Start talking to GHCi

--- server/Main.js
+++ server/Main.js
@@ -1,9 +1,27 @@
 /* eslint-disable camelcase, id-length */
 'use strict';
 
+const childProcess = require('child_process');
+const url = require('url');
 const vscode = require('vscode-languageserver');
 
-exports.connection_create = () => vscode.createConnection();
+exports.childProcess_onClose = (x) => (f) => () =>
+  x.on('close', (code) => f(code)());
+
+exports.childProcess_onStderr = (x) => (f) => () =>
+  x.stderr.on('data', (data) => f(data.toString())());
+
+exports.childProcess_onStdout = (x) => (f) => () =>
+  x.stdout.on('data', (data) => f(data.toString())());
+
+exports.childProcess_spawn = (x) => (xs) => () =>
+  childProcess.spawn(x, xs);
+
+exports.childProcess_writeStdin = (x) => (s) => () =>
+  x.stdin.write(`${s}\n`);
+
+exports.connection_create = () =>
+  vscode.createConnection();
 
 exports.connection_listen = (connection) => () =>
   connection.listen();
@@ -14,10 +32,27 @@ exports.connection_onDidSaveTextDocument = (connection) => (f) => () =>
 exports.connection_onInitialize = (connection) => (f) => () =>
   connection.onInitialize(f);
 
-exports.io_bind = (x) => (f) => () => f(x())();
+exports.intToString = (x) =>
+  x.toString();
 
-exports.io_map = (f) => (x) => () => f(x());
+exports.io_bind = (x) => (f) => () =>
+  f(x())();
 
-exports.io_pure = (x) => () => x;
+exports.io_map = (f) => (x) => () =>
+  f(x());
 
-exports.log = (x) => () => console.log(x);
+exports.io_pure = (x) => () =>
+  x;
+
+exports.log = (x) => () =>
+  console.log(x);
+
+exports.string_append = (x) => (y) =>
+  x + y;
+
+exports.throw = (x) => () => {
+  throw new Error(x);
+};
+
+exports.uriToPath = (x) =>
+  url.fileURLToPath(x);
--- server/Main.purs
+++ server/Main.purs
@@ -15,6 +15,17 @@ newtype Uri = Uri String
 uriToString :: Uri -> String
 uriToString (Uri string) = string
 
+newtype Path = Path String
+
+pathToString :: Path -> String
+pathToString (Path string) = string
+
+foreign import uriToPath :: Uri -> Path
+
+foreign import intToString :: Int -> String
+
+foreign import string_append :: String -> String -> String
+
 foreign import data IO :: Type -> Type
 
 foreign import io_bind :: forall a b . IO a -> (a -> IO b) -> IO b
@@ -41,6 +52,29 @@ foreign import connection_onInitialize
 
 foreign import log :: String -> IO Unit
 
+foreign import throw :: forall a . String -> IO a
+
+foreign import data ChildProcess :: Type
+
+foreign import childProcess_onClose
+  :: ChildProcess
+  -> (Int -> IO Unit)
+  -> IO Unit
+
+foreign import childProcess_onStderr
+  :: ChildProcess
+  -> (String -> IO Unit)
+  -> IO Unit
+
+foreign import childProcess_onStdout
+  :: ChildProcess
+  -> (String -> IO Unit)
+  -> IO Unit
+
+foreign import childProcess_spawn :: String -> Array String -> IO ChildProcess
+
+foreign import childProcess_writeStdin :: ChildProcess -> String -> IO Unit
+
 class HasMap t where
   map :: forall a b . (a -> b) -> t a -> t b
 
@@ -62,15 +96,73 @@ instance ioHasBind :: HasBind IO where
 discard :: forall t b . HasBind t => t Unit -> (Unit -> t b) -> t b
 discard = bind
 
+class HasEmpty t where
+  empty :: t
+
+instance stringHasEmpty :: HasEmpty String where
+  empty = ""
+
+class HasAppend t where
+  append :: t -> t -> t
+
+infixr 6 append as <>
+
+instance stringHasAppend :: HasAppend String where
+  append = string_append
+
 main :: IO Unit
 main = do
-  connection <- connection_create
+  ghci <- childProcess_spawn "stack"
+    -- Separate from GHC, Stack tries to colorize its messages. We don't try to
+    -- parse Stack's output, so it doesn't really matter. But it's annoying to
+    -- see the ANSI escape codes in the debug output.
+    [ "--color=never"
+    -- Explicitly setting the terminal width avoids a warning about `stty`.
+    , "--terminal-width=0"
+    , "exec"
+    , "--"
+    , "ghci"
+    -- This one is critical. Rather than trying to parse GHC's human-readable
+    -- output, we can get it to print out JSON instead. Note that the
+    -- messages themselves are still human readable. It's the metadata that
+    -- gets turned into structured JSON.
+    , "-ddump-json"
+    -- Deferring type errors turns them into warnings, which allows more
+    -- warnings to be reported when there are type errors.
+    , "-fdefer-type-errors"
+    -- We're not interested in actually building anything, just type
+    -- checking. This has the nice side effect of making things faster.
+    , "-fno-code"
+    -- Using multiple cores should be faster. Might need to actually
+    -- benchmark this, and maybe expose it as an option.
+    , "-j"
+    ]
 
-  connection_onDidSaveTextDocument connection \ event ->
-    log (uriToString event.textDocument.uri)
+  childProcess_onStderr ghci \ chunk ->
+    log <| "ghci stderr: " <> chunk
+
+  childProcess_onStdout ghci \ chunk ->
+    log <| "ghci stdout: " <> chunk
+
+  childProcess_onClose ghci \ code ->
+    log <| "GHCi closed with code " <> intToString code
+
+  childProcess_writeStdin ghci ":set prompt \"{- purple-yolk -}\""
+  childProcess_writeStdin ghci ":set +c"
+
+  connection <- connection_create
 
   connection_onInitialize connection <| pure
     { capabilities: { textDocumentSync: { save: true } } }
+
+  connection_onDidSaveTextDocument connection \ event -> do
+    let uri = event.textDocument.uri
+    log (uriToString uri)
+    let path = uriToPath event.textDocument.uri
+    let string = pathToString path
+    -- TODO: this is outputting `_URI { ... }` which doesn't seem right
+    log string
+    childProcess_writeStdin ghci <| ":load " <> string
 
   connection_listen connection
   log "Purple Yolk is up and running!"
