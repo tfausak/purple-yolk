2f3efaaa3cf6170851582b1dbe7762e54b338ece
Taylor Fausak <taylor@fausak.me>
2019-12-19 11:22:47 -0500

    Clean up client

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Purple Yolk is a Haskell IDE for Visual Studio Code.
 
 - [ ] Read GHCi command from config.
 - [ ] Show progress in editor UI.
+- [ ] Make JSON parsing more robust.
+- [ ] Add a command to restart GHCi.
--- client/index.js
+++ client/index.js
@@ -1,27 +1,22 @@
 'use strict';
 
+const path = require('path');
 const vscode = require('vscode-languageclient');
 
 module.exports = {
   activate: (context) => {
-    const nodeModule = {
-      module: context.asAbsolutePath('./dist/server.js'),
-      transport: vscode.TransportKind.ipc,
-    };
+    const server = context.asAbsolutePath(path.join('dist', 'server.js'));
     const client = new vscode.LanguageClient(
       'Purple Yolk',
       {
-        debug: nodeModule,
-        run: nodeModule,
+        debug: {
+          module: server,
+          options: { execArgv: ['--inspect=6009', '--nolazy'] },
+          transport: vscode.TransportKind.ipc,
+        },
+        run: { module: server, transport: vscode.TransportKind.ipc },
       },
-      {
-        documentSelector: [
-          {
-            language: 'haskell',
-            scheme: 'file',
-          },
-        ],
-      }
+      { documentSelector: [{ language: 'haskell', scheme: 'file' }] }
     );
     client.start();
   },
