e2a70536aa69ee803bcfd2048cbcc229d99b9d7b
Taylor Fausak <taylor@fausak.me>
2020-09-18 12:36:56 -0400

    Rewrite things using readline

--- server/index.js
+++ server/index.js
@@ -1,19 +1,34 @@
 'use strict';
 
+// The onStdout function needs to be split up.
+
+// Also there needs to be a more efficient way to send diagnostics. It should
+// be possible to know what we sent last and only send things that changed. As
+// it is currently sending diagnostics is roughly O(n^2). Usually the number of
+// diagnostics is low so it doesn't matter.
+
+// Also the severity needs to check for type errors that were deferred. And
+// maybe <interactive> stuff can be sent at a severity below warnings?
+
+// Also there needs to be some way to avoid queueing up a bunch of reload
+// commands when the user saves a bunch of files.
+
 const childProcess = require('child_process');
 const languageServer = require('vscode-languageserver');
 const purpleYolk = require('../package.json');
+const readline = require('readline');
 const url = require('url');
 
 const connection = languageServer.createConnection();
 const diagnostics = {};
+const epoch = Date.now();
 let ghci = null;
-const prompt = `{-${purpleYolk.name}-${purpleYolk.version}-${Date.now()}-}`;
-let stderr = '';
-let stdout = '';
+const prompt = `{- ${purpleYolk.name} ${purpleYolk.version} ${epoch} -}`;
 
-const say = (message) => console.log(`${Date.now()} ${message}`);
-say('Starting ...');
+const say = (message) => {
+  const timestamp = ((Date.now() - epoch) / 1000).toFixed(3);
+  connection.console.info(`${timestamp} ${message}`);
+};
 
 const updateStatus = (message) =>
   connection.sendNotification(
@@ -21,26 +36,10 @@ const updateStatus = (message) =>
     `Purple Yolk: ${message}`
   );
 
-const handleStream = (stream, data, callback) => {
-  let buffer = `${stream}${data}`;
-  /* eslint-disable no-constant-condition */
-  while (true) {
-    const index = buffer.indexOf('\n');
-    if (index === -1) {
-      break;
-    }
-    callback(buffer.substring(0, index));
-    buffer = buffer.substring(index + 1);
-  }
-  return buffer;
-};
-
-const writeStdin = (command) => {
-  if (!ghci) {
-    throw new Error('Tried to run a command but GHCi is not running.');
-  }
-  say(`[stdin] ${command}`);
-  ghci.stdin.write(`${command}\n`);
+const writeStdin = (message) => {
+  say(`[stdin] ${message}`);
+  updateStatus(`Running ${message}`);
+  ghci.stdin.write(`${message}\n`);
 };
 
 const parseJson = (string) => {
@@ -54,127 +53,129 @@ const parseJson = (string) => {
   }
 };
 
+const onStderr = (line) => say(`[stderr] ${line}`);
+
 const sendDiagnostics = () => {
   Object.keys(diagnostics).forEach((key) => {
-    const value = diagnostics[key];
+    const values = Object.values(diagnostics[key]);
     connection.sendDiagnostics({
-      diagnostics: Object.values(value),
+      diagnostics: values,
       uri: key,
     });
+    if (values.length === 0) {
+      delete diagnostics[key];
+    }
   });
 };
 
-/* eslint-disable max-statements */
-const handleJson = (json) => {
-  const { span } = json;
-  if (span === null) {
-    if (json.reason === null && json.severity === 'SevOutput') {
-      const match = json.doc.match(/Compiling .+ \( (.+), /);
-      if (match) {
-        const file = url.pathToFileURL(match[1]);
-        diagnostics[file] = {};
-      }
-    }
-    return;
+const getSeverity = (json) => {
+  if (json.severity === 'SevWarning') {
+    return 2;
   }
-  const file = url.pathToFileURL(span.file);
-  if (!diagnostics[file]) {
-    diagnostics[file] = {};
-  }
-  const key = [
-    span.startLine,
-    span.startCol,
-    span.endLine,
-    span.endCol,
-    json.reason,
-  ].join(' ');
-  diagnostics[file][key] = {
-    code: json.reason,
-    message: json.doc,
-    range: {
-      end: {
-        character: span.endCol - 1,
-        line: span.endLine - 1,
-      },
-      start: {
-        character: span.startCol - 1,
-        line: span.startLine - 1,
-      },
-    },
-    severity: 1,
-    source: purpleYolk.name,
-  };
-  sendDiagnostics();
+  return 1;
 };
 
-const startGhci = () => connection
-  .workspace
-  .getConfiguration(purpleYolk.name)
-  .then((configuration) => {
-    const { command } = configuration.ghci;
-    say(`Starting GHCi with: ${command}`);
-    if (ghci) {
-      throw new Error('Wanted to start GHCi but it\'s already running.');
-    }
-    ghci = childProcess.spawn(command, { shell: true });
-    ghci.on('exit', (code, signal) => {
-      if (code === 0) {
-        say('GHCi exited successfully.');
-      } else {
-        throw new Error(`GHCi exited with code ${code} and signal ${signal}!`);
-      }
-    });
-    ghci.stderr.on('data', (data) => {
-      stderr = handleStream(stderr, data, (line) => say(`[stderr] ${line}`));
-    });
-    ghci.stdout.on('data', (data) => {
-      stdout = handleStream(stdout, data, (line) => {
+/* eslint-disable max-lines-per-function, max-statements */
+const onStdout = (line) => {
+  const json = parseJson(line);
+  if (json) {
+    if (json.span) {
+      if (json.span.file === '<interactive>') {
         say(`[stdout] ${line}`);
-        const json = parseJson(line);
-        if (json) {
-          handleJson(json);
+      } else {
+        const file = url.pathToFileURL(json.span.file);
+        const key = [
+          json.span.startLine,
+          json.span.startCol,
+          json.span.endLine,
+          json.span.endCol,
+          json.reason,
+        ].join(' ');
+        if (!diagnostics[file]) {
+          diagnostics[file] = {};
         }
-      });
-    });
+        diagnostics[file][key] = {
+          code: json.reason,
+          message: json.doc,
+          range: {
+            end: {
+              character: json.span.endCol - 1,
+              line: json.span.endLine - 1,
+            },
+            start: {
+              character: json.span.startCol - 1,
+              line: json.span.startLine - 1,
+            },
+          },
+          severity: getSeverity(json),
+          source: purpleYolk.name,
+        };
+        sendDiagnostics();
+      }
+    } else if (json.reason === null && json.severity === 'SevOutput') {
+      const pattern = /^\[ *(\d+) of (\d+)\] Compiling (\S+) *\( ([^,]+), /;
+      const match = json.doc.match(pattern);
+      if (match) {
+        diagnostics[url.pathToFileURL(match[4])] = {};
+        sendDiagnostics();
+      } else {
+        say(`[stdout] ${line}`);
+      }
+    } else {
+      say(`[stdout] ${line}`);
+    }
+  } else if (line.indexOf(prompt) === -1) {
+    say(`[stdout] ${line}`);
+  } else {
+    updateStatus('Idle');
+  }
+};
+
+const onExit = (code, signal) => {
+  if (code === 0) {
+    say('GHCi exited successfully.');
+  } else {
+    throw new Error(`GHCi exited with ${code} (${signal})!`);
+  }
+};
+
+const startGhci = () => {
+  say('Starting GHCi ...');
+  updateStatus('Starting GHCi');
+  connection.workspace.getConfiguration(purpleYolk.name).then((config) => {
+    const { command } = config.ghci;
+    say(`Spawning GHCi with: ${command}`);
+    ghci = childProcess.spawn(command, { shell: true });
+    ghci.on('exit', onExit);
+    readline.createInterface({ input: ghci.stderr }).on('line', onStderr);
+    readline.createInterface({ input: ghci.stdout }).on('line', onStdout);
     writeStdin(`:set prompt "${prompt}\\n"`);
   });
+};
 
-connection.onInitialize(() => {
-  say('Initializing ...');
-  return {
-    capabilities: {
-      textDocumentSync: {
-        save: {},
-      },
-    },
-  };
-});
+say(`Starting ${purpleYolk.name} version ${purpleYolk.version} ...`);
+
+connection.onInitialize(() =>
+  ({ capabilities: { textDocumentSync: { save: {} } } }));
 
 connection.onInitialized(() => {
   say('Initialized.');
-  updateStatus('Starting GHCi');
   startGhci();
 });
 
 connection.onDidSaveTextDocument((params) => {
-  say(`Saved ${params.textDocument.uri}`);
+  say(`Saved ${params.textDocument.uri}.`);
   writeStdin(':reload');
 });
 
 connection.onNotification(`${purpleYolk.name}/restartGhci`, () => {
   say('Stopping GHCi ...');
   updateStatus('Stopping GHCi');
-  ghci.kill();
   ghci.on('exit', () => {
-    say('Starting GHCi ...');
-    updateStatus('Starting GHCi');
+    ghci = null;
     startGhci();
   });
-  Object.keys(diagnostics).forEach((key) => {
-    diagnostics[key] = [];
-  });
-  sendDiagnostics();
-  ghci = null;
+  ghci.kill();
 });
 
 connection.listen();
