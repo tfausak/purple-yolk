c9ac80b96d827e993f70a1ab91f4ae670fd80433
Taylor Fausak <taylor@fausak.me>
2020-09-27 10:51:04 -0400

    Start working on new client and server

--- client/index.js
+++ client/index.js
@@ -2,80 +2,43 @@
 
 const lsp = require('vscode-languageclient');
 const path = require('path');
-const purpleYolk = require('../package.json');
+const py = require('../package.json');
 const vscode = require('vscode');
 
-const serverOptions = (context) => {
-  const server = context.asAbsolutePath(path.join('dist', 'server.js'));
-  return {
-    debug: {
-      module: server,
-      options: { execArgv: ['--inspect=6009', '--nolazy'] },
-      transport: lsp.TransportKind.ipc,
+const activate = (context) => {
+  const outputChannel = vscode.window.createOutputChannel(py.displayName);
+
+  const client = new lsp.LanguageClient(
+    py.displayName,
+    {
+      run: {
+        module: context.asAbsolutePath(path.join('dist', 'server.js')),
+        transport: lsp.TransportKind.ipc,
+      },
     },
-    run: { module: server, transport: lsp.TransportKind.ipc },
-  };
+    {
+      documentSelector: [{ language: 'haskell', scheme: 'file' }],
+      outputChannel,
+    }
+  );
+
+  context.subscriptions.push(vscode.commands.registerCommand(
+    `${py.name}.restart`,
+    () => client.sendNotification(`${py.name}/restart`, null)
+  ));
+
+  context.subscriptions.push(vscode.commands.registerCommand(
+    `${py.name}.showOutput`,
+    () => outputChannel.show(true)
+  ));
+
+  const statusBarItem = vscode.window.createStatusBarItem();
+  statusBarItem.command = `${py.name}.showOutput`;
+  statusBarItem.text = py.displayName;
+  statusBarItem.tooltip = 'Click to show output.';
+  statusBarItem.show();
+
+  client.start();
 };
 
-const clientOptions = (outputChannel) => ({
-  documentSelector: [{ language: 'haskell', scheme: 'file' }],
-  outputChannel,
-});
-
-const onRestartCommand = (client) =>
-  client.sendNotification(`${purpleYolk.name}/restartGhci`, null);
-
-const onShowOutputCommand = (outputChannel) => outputChannel.show(true);
-
-const onUpdateStatusBarItemNotification = (statusBarItem, text) => {
-  statusBarItem.text = text;
-};
-
-module.exports = {
-  activate: (context) => {
-    const outputChannel = vscode.window
-      .createOutputChannel(purpleYolk.displayName);
-
-    const client = new lsp.LanguageClient(
-      purpleYolk.displayName,
-      serverOptions(context),
-      clientOptions(outputChannel)
-    );
-
-    client.start();
-
-    context.subscriptions.push(vscode.commands.registerCommand(
-      `${purpleYolk.name}.restart`,
-      () => onRestartCommand(client)
-    ));
-
-    context.subscriptions.push(vscode.commands.registerCommand(
-      `${purpleYolk.name}.showOutput`,
-      () => onShowOutputCommand(outputChannel)
-    ));
-
-    const statusBarItem = vscode.window.createStatusBarItem();
-    statusBarItem.command = `${purpleYolk.name}.showOutput`;
-    statusBarItem.text = `${purpleYolk.displayName}: Initializing`;
-    statusBarItem.show();
-
-    client.onReady().then(() => {
-      vscode.window.withProgress({
-        location: vscode.ProgressLocation.Notification,
-        title: purpleYolk.displayName,
-      }, (progress) => {
-        client.onNotification(
-          `${purpleYolk.name}/updateStatusBarItem`,
-          (text) => onUpdateStatusBarItemNotification(statusBarItem, text)
-        );
-
-        client.onNotification(
-          `${purpleYolk.name}/updateProgress`,
-          (text) => progress.report({ message: text })
-        );
-
-        return new Promise(() => {});
-      });
-    });
-  },
-};
+module.exports = { activate };
--- server/index.js
+++ server/index.js
@@ -1,235 +1,86 @@
 'use strict';
 
-// There needs to be some way to avoid queueing up a bunch of reload commands
-// when the user saves a bunch of files.
-
 const childProcess = require('child_process');
-const languageServer = require('vscode-languageserver');
-const purpleYolk = require('../package.json');
+const lsp = require('vscode-languageserver');
+const py = require('../package.json');
 const readline = require('readline');
-const url = require('url');
 
-const connection = languageServer.createConnection();
-const diagnostics = {};
+const connection = lsp.createConnection();
 const epoch = Date.now();
 let ghci = null;
-const prompt = `{- ${purpleYolk.name} ${purpleYolk.version} ${epoch} -}`;
+const prompt = `{- ${py.name} ${py.version} ${epoch} -}`;
 
 const say = (message) => {
   const timestamp = ((Date.now() - epoch) / 1000).toFixed(3);
   connection.console.info(`${timestamp} ${message}`);
 };
 
-const updateStatus = (message) =>
-  connection.sendNotification(
-    `${purpleYolk.name}/updateStatusBarItem`,
-    `${purpleYolk.displayName}: ${message}`
-  );
-
-const updateProgress = (message) =>
-  connection.sendNotification(`${purpleYolk.name}/updateProgress`, message);
-
-const writeStdin = (message) => {
-  say(`[stdin] ${message}`);
-  updateStatus(`Running ${message}`);
-  ghci.stdin.write(`${message}\n`);
+const tellGhci = (command) => {
+  say(`[stdin] ${command}`);
+  ghci.stdin.write(`${command}\n`);
 };
 
-const parseJson = (string) => {
-  try {
-    return JSON.parse(string);
-  } catch (error) {
-    if (error instanceof SyntaxError) {
-      return null;
-    }
-    throw error;
-  }
-};
+const startGhciWith = (command) => {
+  say(`Starting GHCi: ${command}`);
 
-const onStderr = (line) => say(`[stderr] ${line}`);
+  ghci = childProcess.spawn(command, { shell: true });
 
-const sendDiagnostics = (file) => {
-  const value = diagnostics[file];
-  if (!value) {
-    return;
-  }
-  const values = Object.values(value);
-  connection.sendDiagnostics({
-    diagnostics: values,
-    uri: file,
+  ghci.on('error', (err) => {
+    throw err;
   });
-  if (values.length === 0) {
-    delete diagnostics[file];
-  }
-};
 
-const clearDiagnostics = () => Object.keys(diagnostics).forEach((key) => {
-  diagnostics[key] = {};
-  sendDiagnostics(key);
-});
-
-const onStdoutLine = (line) => {
-  if (line.indexOf(prompt) === -1) {
-    say(`[stdout] ${line}`);
-  } else {
-    updateStatus('Idle');
-  }
-};
-
-const getSeverity = (json) => {
-  switch (json.severity) {
-    case 'SevError': return languageServer.DiagnosticSeverity.Error;
-    case 'SevWarning': switch (json.reason) {
-      case 'Opt_WarnDeferredOutOfScopeVariables':
-        return languageServer.DiagnosticSeverity.Error;
-      case 'Opt_WarnDeferredTypeErrors':
-        return languageServer.DiagnosticSeverity.Error;
-      default: switch (json.span && json.span.file) {
-        case '<interactive>':
-          return languageServer.DiagnosticSeverity.Information;
-        default: return languageServer.DiagnosticSeverity.Warning;
-      }
+  ghci.on('exit', (code, signal) => {
+    if (!ghci.killed) {
+      throw new Error(`GHCi exited with code ${code} and signal ${signal}!`);
     }
-    default: return languageServer.DiagnosticSeverity.Information;
-  }
-};
+    say(`Killed GHCi (${signal})`);
+  });
 
-const getRange = (json) => {
-  const range = {
-    end: {
-      character: 0,
-      line: 0,
-    },
-    start: {
-      character: 0,
-      line: 0,
-    },
-  };
+  readline.createInterface({ input: ghci.stderr }).on('line', (line) => {
+    say(`[stderr] ${line}`);
+  });
 
-  if (json.span) {
-    range.start.line = json.span.startLine - 1;
-    range.start.character = json.span.startCol - 1;
-    range.end.line = json.span.endLine - 1;
-    range.end.character = json.span.endCol - 1;
-  }
+  readline.createInterface({ input: ghci.stdout }).on('line', (line) => {
+    say(`[stdout] ${line}`);
+  });
 
-  return range;
-};
-
-const getFile = (json) => {
-  if (json.span && json.span.file !== '<interactive>') {
-    return url.pathToFileURL(json.span.file);
-  }
-
-  return url.pathToFileURL('.');
-};
-
-const onOutput = (line, json) => {
-  const pattern = /^\[ *(\d+) of (\d+)\] Compiling (\S+) *\( ([^,]+), /;
-  const match = json.doc.match(pattern);
-  if (!match) {
-    return onStdoutLine(line);
-  }
-
-  updateProgress(`${match[1]} of ${match[2]}: ${match[3]}`);
-
-  const file = url.pathToFileURL(match[4]);
-  diagnostics[file] = {};
-  return sendDiagnostics(file);
-};
-
-const onStdoutJson = (line, json) => {
-  if (
-    json.span === null &&
-    json.span === null &&
-    json.severity === 'SevOutput'
-  ) {
-    return onOutput(line, json);
-  }
-
-  const file = getFile(json);
-  const range = getRange(json);
-
-  const key = [
-    range.start.line,
-    range.start.character,
-    range.end.line,
-    range.end.character,
-    json.reason,
-  ].join(' ');
-
-  if (!diagnostics[file]) {
-    diagnostics[file] = {};
-  }
-
-  diagnostics[file][key] = {
-    code: json.reason,
-    message: json.doc,
-    range,
-    severity: getSeverity(json),
-    source: purpleYolk.name,
-  };
-
-  return sendDiagnostics(file);
-};
-
-const onStdout = (line) => {
-  const json = parseJson(line);
-  if (json) {
-    onStdoutJson(line, json);
-  } else {
-    onStdoutLine(line);
-  }
-};
-
-const onExit = (code, signal) => {
-  if (code === 0) {
-    say('GHCi exited successfully.');
-  } else if (ghci.killed) {
-    say(`GHCi killed with ${code} (${signal}).`);
-  } else {
-    throw new Error(`GHCi exited with ${code} (${signal})!`);
-  }
+  tellGhci(`:set prompt "${prompt}\\n"`);
 };
 
 const startGhci = () => {
-  say('Starting GHCi ...');
-  updateStatus('Starting GHCi');
-  connection.workspace.getConfiguration(purpleYolk.name).then((config) => {
-    const { command } = config.ghci;
-    say(`Spawning GHCi with: ${command}`);
-    ghci = childProcess.spawn(command, { shell: true });
-    ghci.on('exit', onExit);
-    readline.createInterface({ input: ghci.stderr }).on('line', onStderr);
-    readline.createInterface({ input: ghci.stdout }).on('line', onStdout);
-    writeStdin(`:set prompt "${prompt}\\n"`);
-  });
+  if (ghci) {
+    throw new Error('Attempted to start GHCi but it is already running!');
+  }
+
+  connection.workspace.getConfiguration(py.name)
+    .then((config) => startGhciWith(config.ghci.command));
 };
 
-say(`Starting ${purpleYolk.name} version ${purpleYolk.version} ...`);
+const restartGhci = () => {
+  ghci.on('exit', () => {
+    ghci = null;
+    startGhci();
+  });
 
-connection.onInitialize(() =>
-  ({ capabilities: { textDocumentSync: { save: {} } } }));
+  ghci.kill();
+};
+
+connection.onInitialize(() => {
+  say(`Initializing ${py.name} ${py.version}`);
+  return { capabilities: { textDocumentSync: { save: true } } };
+});
 
 connection.onInitialized(() => {
-  say('Initialized.');
   startGhci();
 });
 
 connection.onDidSaveTextDocument((params) => {
-  say(`Saved ${params.textDocument.uri}.`);
-  writeStdin(':reload');
+  say(`Saved ${params.textDocument.uri}`);
+  tellGhci(':reload');
 });
 
-connection.onNotification(`${purpleYolk.name}/restartGhci`, () => {
-  say('Stopping GHCi ...');
-  updateStatus('Stopping GHCi');
-  ghci.on('exit', () => {
-    ghci = null;
-    clearDiagnostics();
-    startGhci();
-  });
-  ghci.kill();
+connection.onNotification(`${py.name}/restart`, () => {
+  restartGhci();
 });
 
 connection.listen();
